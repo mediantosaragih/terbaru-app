646925a8ca31102c9258a98b9507ca1b1619161c
mediantosaragih001 <medianto.s@literasia.co.id>
2021-12-08 10:34:49 +0700

    update

---
+++ pages/index.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <h1 style="text-align: center;margin-top: 40px;padding: 70px;color: #737373;">
+          LiTERASIA
+          <span>EduTekno Digital</span>
+        </h1>
+      </div>
+      <div class="col-md-7" style="padding-left: 200px; text-align: justify">
+        <h4 style="color: #737373">
+          PT Literasia Edutekno Digital adalah perusahaan App Developer, Web
+          Developers dan IT Solution. Berawal pada tahun 2018, perusahaan
+          mengeluarkan produk aplikasi sistem manajemen sekolah. Kemudian, pada
+          tahun 2020, perusahaan mengembangkan aplikasi dengan brand Literasia.
+          Aplikasi ini merupakan platform yang didalamnya mencakup perpustakaan
+          digital, media pembelajaran digital dan sistem manajemen sekolah dalam
+          satu aplikasi. PT Literasia Edutekno Digital telah terdaftar di
+          Kemenhumkam berdasarkan Akte Pendirian No 26 pada tanggal 26 Desember
+          2020 dan berkedudukan di Medan Sumatera Utara.
+        </h4>
+      </div>
+      <div class="col-md-5">
+        <img
+          src="../assets/img/hero.png"
+          alt="Logo"
+          style="width: 400px; height: 300px"
+        />
+      </div>
+    </div>
+    <div class="row" style="margin-top: 100px">
+      <div class="col-md-5" style="padding-left: 200px">
+        <img
+          src="../assets/img/hero.png"
+          alt="Logo"
+          style="width: 400px; height: 300px"
+        />
+      </div>
+      <div
+        class="col-md-7"
+        style="padding-left: 50px; padding-right: 100px; text-align: justify"
+      >
+        <h4 style="color: #737373">
+          Berdasarkan data Unesco, Indonesia menempati peringkat 62 dari 70
+          negara dengan minat baca terendah di dunia. Dan dengan survey yang
+          sama, presentase minat baca Indonesia sebesar 0,001%. Dimana dari 1000
+          orang hanya 1 yang memiliki minat baca. Berdasarkan keadaan di
+          lapangan, dampak dari pandemi covid 19 mengakibatkan menurunnya
+          kualitas belajar mengajar di sekolah dan terputusnya komunikasi antara
+          guru dan siswa. Selain daripada itu, banyaknya penerbit buku fisik
+          yang tutup akibat persaingan dan kemajuan dari teknologi digital.
+          Berdasarka problem diatas, PT Literasia Edutekno Digital memberikan
+          solusi untuk menjawab tantangan tersebut dengan menciptakan produk
+          literasia.
+        </h4>
+      </div>
+    </div>
+    <div class="row" style="margin-top: 100px">
+      <div class="col-md-7" style="padding-left: 200px; text-align: justify">
+        <h4 style="color: #737373">
+          PT Literasia Edutekno Digital adalah perusahaan App Developer, Web
+          Developers dan IT Solution. Berawal pada tahun 2018, perusahaan
+          mengeluarkan produk aplikasi sistem manajemen sekolah. Kemudian, pada
+          tahun 2020, perusahaan mengembangkan aplikasi dengan brand Literasia.
+          Aplikasi ini merupakan platform yang didalamnya mencakup perpustakaan
+          digital, media pembelajaran digital dan sistem manajemen sekolah dalam
+          satu aplikasi. PT Literasia Edutekno Digital telah terdaftar di
+          Kemenhumkam berdasarkan Akte Pendirian No 26 pada tanggal 26 Desember
+          2020 dan berkedudukan di Medan Sumatera Utara.
+        </h4>
+      </div>
+      <div class="col-md-5">
+        <img
+          src="../assets/img/hero.png"
+          alt="Logo"
+          style="width: 400px; height: 300px"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.navbar {
+  margin-top: 10px;
+}
+main {
+  background-color: red;
+}
+body {
+  background-image: url("../assets/img/bg-1.jpg");
+  background-size: 100%;
+  background-repeat: repeat-x;
+}
+</style>
