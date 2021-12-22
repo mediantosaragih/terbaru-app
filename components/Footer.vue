4cc5c4c00f8d895e2f65ecf9a62cb5d1881183ea
tinapardede01 <tinapardede@literasia.co.id>
2021-12-22 15:19:10 +0700

    up

--- components/Footer.vue
+++ components/Footer.vue
@@ -89,7 +89,7 @@
 
         <!-- Grid column -->
         <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-          <!-- Link -->
+          <!-- Links -->
                     <img
             src="../assets/img/logo.png"
             alt="Logo"
